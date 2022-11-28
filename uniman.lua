d4ac769498cbc49e4dbb622518345ee72758b8c9
Skivux <118767507+Skivux@users.noreply.github.com>
2022-11-28 02:31:20 -0600

    Update uniman.lua

--- uniman.lua
+++ uniman.lua
@@ -1,4 +1,3 @@
-_G.wl_key="KEY HERE"
 local places = {
     [2788229376] = "https://raw.githubusercontent.com/Skivux/uni/main/whitelist_output%20(2).lua",
     [9825515356] = "https://raw.githubusercontent.com/Skivux/hoodcustoms/main/SkiCustoms.lua"
